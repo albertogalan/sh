56f8efd932c9e707631fe63ae79449656d65dde2
Alberto Galan <albertogalan@users.noreply.github.com>
2021-12-11 15:27:40 +0000

    Update connectnewserver.sh

--- sh/scripts/connectnewserver.sh
+++ sh/scripts/connectnewserver.sh
@@ -43,6 +43,7 @@ test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bash_profile
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.profile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Add Developing Tools
 brew update
