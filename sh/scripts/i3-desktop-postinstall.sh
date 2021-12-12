25d232cc6d23e75c98b20b8060849e73640ebfa7
agalan <agalan75@web.de>
2021-12-12 16:47:12 +0000

    develop adding dotvim repo

--- sh/scripts/i3-desktop-postinstall.sh
+++ sh/scripts/i3-desktop-postinstall.sh
@@ -1,17 +1,11 @@
 #!/bin/bash 
 
+
 repos="
 docs
+cheatsheet
+dotvim
 "
-
-adding_dotfiles(){
-
-## install dot files
-mkdir -p  /home/agalan/.config
-git clone https://github.com/albertogalan/sh.git /tmp/sh
-rsync -a /tmp/sh/dot/  /home/agalan/.config/
-
-}
 
 
 cloning_repos(){
@@ -21,11 +15,13 @@ ssh-add /home/agalan/.ssh/agalan-github-key
 
 for repo in $repos
 do
-git clone git@github.com:albertogalan/$repo.git
+git clone git@github.com:albertogalan/$repo.git /data/src
 done
 
 }
 
-adding_dotfiles
-cloning_repos
+sync_dot(){
 
+rsync -a /data/src/dotvim/  /home/agalan/.vim/
+
+}
