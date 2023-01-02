7ab2f1ed06a1add278699b2ad336acd2ebf5e74d
A Galan <info@albertogalan.com>
2023-01-02 15:20:25 +0000

    tunnel

--- provision/minikube/start.sh
+++ provision/minikube/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
-minikube start --kubernetes-version v1.21.0 --memory 8192 --cpus 3 --extra-config=apiserver.v=10 --extra-config=kubelet.max-pods=100
+minikube start --kubernetes-version v1.22.0 --memory 8192 --cpus 3 --extra-config=apiserver.v=10 --extra-config=kubelet.max-pods=100
 minikube addons enable ingress 
 minikube addons enable registry-creds
 minikube addons enable metrics-server
+minikube tunnel &
 #minikube addons enable istio #
 
